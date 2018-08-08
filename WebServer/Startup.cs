8479d040fc7f1c1775d0ef169b2d5e7044b8943b
vsoff <rucamper@mail.ru>
2018-08-08 12:55:43 +0300

    Инициализирован проект веб-сервера

---
+++ WebServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebServer.Startup))]
+namespace WebServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
