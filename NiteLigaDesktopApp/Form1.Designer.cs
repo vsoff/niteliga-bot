94c21f7eedb56a4262cd91d0a754c08b605a425d
vsoff <rucamper@mail.ru>
2019-01-17 20:42:32 +0300

    Сброшен конфиг в старом проекте, тестовый проект обновлён для тестирования

--- NiteLigaDesktopApp/Form1.Designer.cs
+++ NiteLigaDesktopApp/Form1.Designer.cs
@@ -29,23 +29,34 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 100);
+            this.button1.Location = new System.Drawing.Point(12, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(316, 79);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(316, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 239);
+            this.ClientSize = new System.Drawing.Size(340, 130);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- NiteLigaDesktopApp/Form1.cs
+++ NiteLigaDesktopApp/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using NL.Core;
 using NL.NiteLiga.Core;
+using NL.NiteLiga.Core.DataAccess.Entites;
 using NL.NiteLiga.Core.DataAccess.Repositories;
+using NL.NiteLiga.Core.Game;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +10,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Unity;
@@ -16,20 +19,55 @@ namespace NiteLigaDesktopApp
 {
     public partial class Form1 : Form
     {
+        private readonly IUnityContainer _container;
+        private NiteLigaGameManager _gameManager;
+        private GameTemplate[] _templates;
+
         public Form1()
         {
             InitializeComponent();
+
+            _container = new UnityContainer();
+            NiteLigaCoreModule.Register(_container);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (var container = new UnityContainer())
+            int templateIndex = comboBox1.SelectedIndex;
+            if (templateIndex == -1)
             {
-                NiteLigaCoreModule.Register(container);
-                var teamsRepository = container.Resolve<ITeamsRepository>();
-                var a = teamsRepository.GetTeam(1);
-                var aa = new TeamsRepository(null);
+                MessageBox.Show("Не выбрано ни одной игры");
+                return;
             }
+
+            var gamesRepos = _container.Resolve<IGamesRepository>();
+            var template = gamesRepos.GetTemplateHard(_templates[templateIndex].Id);
+            _gameManager = _container.Resolve<NiteLigaGameManager>();
+            _gameManager.Start(new NiteLigaGameConfiguration
+            {
+                GameMatchId = 0,
+                Config = template.Config,
+                Settings = template.Settings
+            });
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    _gameManager.Iterate();
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            var gamesRepository = _container.Resolve<IGamesRepository>();
+            _templates = gamesRepository.GetAllTemplatesLight();
+
+            comboBox1.Items.Clear();
+            foreach (var template in _templates)
+                comboBox1.Items.Add($"{template.Id}. {template.Caption}");
         }
     }
 }
